64f652da003d78f7206f9a283573c7c356b66810
Ankit Khandeparkar <ankit.khandeparkar@gmail.com>
2023-07-17 19:23:47 +0200

    LED BLINK NO LIBRARY

--- LED_BLINK_NO_LIBRARY/include/STM32_GPIO.h
+++ LED_BLINK_NO_LIBRARY/include/STM32_GPIO.h
@@ -1,8 +1,26 @@
 #ifndef STM32_GPIO_H
 #define STM32_GPIO_H
 
+#define PERIPH_BASE 0x40000000UL
+#define AHB1_OFFSET 0X00020000UL
+#define AHB1_BASE PERIPH_BASE + AHB1_OFFSET
 
+#define GPIOC_OFFSET 0X00000800UL
+#define GPIOC AHB1_BASE + GPIOC_OFFSET
 
+#define RCC_OFFSET 0X00003800UL
+#define RCC_BASE AHB1_BASE + RCC_OFFSET
 
+#define RCC_AHB1ENR_OFFSET 0X30UL
+#define RCC_AHB1ENR *(volatile unsigned int *)(RCC_BASE + RCC_AHB1ENR_OFFSET)
+#define GPIOCEN 1U << 2
 
-#endif
+#define GPIOC_MODER_OFFSET 0X00UL
+#define GPIOC_MODER *(volatile unsigned int *)(GPIOC + GPIOC_MODER_OFFSET)
+
+#define GPIOC_ODR_OFFSET 0X14UL
+#define GPIOC_ODR *(volatile unsigned int *)(GPIOC + GPIOC_ODR_OFFSET)
+
+#define LED_PIN 1U << 13
+
+#endif /* STM32_GPIO_H */
--- LED_BLINK_NO_LIBRARY/src/main.c
+++ LED_BLINK_NO_LIBRARY/src/main.c
@@ -1,11 +1,18 @@
 #include "STM32_GPIO.h"
 
-
 int main(void)
 {
+    RCC_AHB1ENR |= GPIOCEN;
 
+    GPIOC_MODER |= 1U << 26;
+    GPIOC_MODER &= ~(1U << 27);
 
-
+    while (1)
+    {
+        GPIOC_ODR ^= LED_PIN;
+        for (volatile int i = 0; i < 1000000; i++)
+            ;
+    }
 
     return 0;
 }
